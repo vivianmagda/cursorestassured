481d23b49185f4781278ba24e547dd46c7c6647e
vivianmagda <vivianmagda@live.com>
2025-08-06 17:47:07 -0300

    first commit

---
+++ cursorestassured/src/test/java/OlaMundoTest.java
@@ -0,0 +1,25 @@
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
+public class OlaMundoTest {
+
+    @Test
+    public void testOlaMundo(){
+      
+       Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+       Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+       Assert.assertTrue(response.statusCode() == 200);
+       Assert.assertTrue("Status code esperado: 201", response.statusCode() == 200);
+       Assert.assertEquals(201, response.statusCode());
+
+       ValidatableResponse validacao = response.then();
+       validacao.statusCode(200);          
+    }
+
+}
