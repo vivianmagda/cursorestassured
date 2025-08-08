1e060a2463ac960908e99ac7426c2f8978904ba4
vivianmagda <vivianmagda@live.com>
2025-08-08 15:29:04 -0300

    [BDD and Matchers]

--- cursorestassured/src/test/java/OlaMundoTest.java
+++ cursorestassured/src/test/java/OlaMundoTest.java
@@ -1,7 +1,19 @@
 
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.request;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
+
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
@@ -10,16 +22,45 @@ import io.restassured.response.ValidatableResponse;
 public class OlaMundoTest {
 
     @Test
-    public void testOlaMundo(){
-      
-       Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
-       Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
-       Assert.assertTrue(response.statusCode() == 200);
-       Assert.assertTrue("Status code esperado: 201", response.statusCode() == 200);
-       Assert.assertEquals(201, response.statusCode());
+    public void testOlaMundo() {
 
-       ValidatableResponse validacao = response.then();
-       validacao.statusCode(200);          
+        Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+        Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+        Assert.assertTrue(response.statusCode() == 200);
+        Assert.assertTrue("Status code esperado: 201", response.statusCode() == 200);
+        Assert.assertEquals(200, response.statusCode());
+
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
+    }
+
+    @Test
+    public void devoConhecerOutrasFormasRestAssured() {
+
+        Response response = request(Method.GET, "https://restapi.wcaquino.me/ola");
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
+
+        get("https://restapi.wcaquino.me/ola").then().statusCode(200);
+
+        given()
+            //Pré condições
+        .when()
+            .get("https://restapi.wcaquino.me/ola")
+        .then()
+            .statusCode(200);
+
+    }
+
+    @Test
+    public void devoConhecerMatchersHamcrest(){
+        assertThat("Maria", Matchers.is("Maria"));        
+        assertThat(128%3, Matchers.not(0));
+
+        List<Integer> impares = Arrays.asList(1,3,5,7,9);
+        assertThat(impares, hasSize(5));
+        assertThat(impares, contains(1,3,5,7,9));
+        assertThat(impares, hasItem(2));
     }
 
 }
