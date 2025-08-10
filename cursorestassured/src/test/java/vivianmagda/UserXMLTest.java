4a0a742cbe94de1f64a26143038539e40ce115ed
vivianmagda <vivianmagda@live.com>
2025-08-10 16:30:40 -0300

    unindo xmlPath com JAVA

--- cursorestassured/src/test/java/vivianmagda/UserXMLTest.java
+++ cursorestassured/src/test/java/vivianmagda/UserXMLTest.java
@@ -2,10 +2,15 @@ package vivianmagda;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.Test;
+
+import io.restassured.path.xml.element.Node;
 
 public class UserXMLTest {
 
@@ -45,6 +50,21 @@ public class UserXMLTest {
                 .body("salary.find{it != null}.toDouble()", is(1234.5678d))
                 .body("name.findAll{it.toString().startsWith('Maria')}.collect{it.toString().toUpperCase()}", is("MARIA JOAQUINA"))
         ;
+    }
+
+    @Test
+    public void devoFazerPesquisasAvancadascomXMLeJAVA(){
+        ArrayList<Node> nomes = given()
+        .when()
+            .get("https://restapi.wcaquino.me/usersXML/")
+        .then()
+            .statusCode(200)
+            .extract().path("users.user.name.findAll{it.toString().contains('n')}")
+        ;
+        assertEquals(2, nomes.size());
+        assertTrue("Maria Joaquina".equalsIgnoreCase(nomes.get(0).toString()));
+        assertTrue("Ana Julia".equalsIgnoreCase(nomes.get(1).toString()));
+
     }
 
 }
