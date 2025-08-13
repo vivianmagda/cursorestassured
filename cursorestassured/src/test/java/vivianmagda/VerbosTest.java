8d6e3298207c3de43f261f1286a374938be589fe
vivianmagda <vivianmagda@live.com>
2025-08-12 21:42:20 -0300

    [validacao ao salvar]

--- cursorestassured/src/test/java/vivianmagda/VerbosTest.java
+++ cursorestassured/src/test/java/vivianmagda/VerbosTest.java
@@ -3,12 +3,18 @@ package vivianmagda;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
+import java.beans.Encoder;
+
 import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.config.EncoderConfig;
 
 public class VerbosTest {
 
     @Test
     public void deveSalvarUsuario(){
+
         given()
             .log().all()
             .contentType("application/json")
@@ -22,6 +28,25 @@ public class VerbosTest {
             .body("name", is("Jose"))            
             .body("age", is("50"))
         ;
+    }
+
+    @Test
+    public void naoDeveSalvarUsuarioSemNome(){
+        RestAssured.config = RestAssured.config()
+            .encoderConfig(EncoderConfig.encoderConfig().defaultContentCharset("UTF-8"));
+        given()
+            .log().all()
+            .contentType("application/json")
+            .body("{\"age\" : \"50\"}")
+        .when()
+            .post("https://restapi.wcaquino.me/users")
+        .then()
+             .log().all()
+            .statusCode(400)
+            .body("id", is(nullValue()))
+            .body("error", is("Name é um atributo obrigatório"))
+        ;
+
     }
 
 }
