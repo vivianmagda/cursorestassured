86b50e19e3800f1206c5baa27b11569255d34191
vivianmagda <vivianmagda@live.com>
2025-08-13 22:36:32 -0300

    [query]

---
+++ cursorestassured/src/test/java/vivianmagda/EnvioDadosTest.java
@@ -0,0 +1,24 @@
+package vivianmagda;
+
+import static io.restassured.RestAssured.given;
+
+import org.junit.Test;
+
+import io.restassured.http.ContentType;
+
+public class EnvioDadosTest {
+
+    @Test
+    public void deveEnviarValorViaQuery(){
+        given()
+            .log().all()
+        .when()
+            .get("https://restapi.wcaquino.me/v2/users?format=xml")
+        .then()
+            .log().all()
+            .statusCode(200)
+            .contentType(ContentType.XML)
+        ;
+    }
+
+}
